644bf7908b9571e42e74e229d03870076764c193
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-10-27 10:17:06 -0400

    began overriding theme configs

---
+++ content/content/Lessons-Learned-Setting-Up-Pelican.md
@@ -0,0 +1,10 @@
+Title: Lessons Learned Setting up Pelican
+Date: 2024-10-27 10:15
+Category: Lessons-Learned
+
+Pelican is a python website build & template library. Using it is fairly simple as all dependencies
+are python libraries, so no complex environment management is necessary.
+
+![Lessons-Learned-Setting-Up-Pelican-screenshot.png]({filename}/images/Lessons-Learned-Setting-Up-Pelican-screenshot.png)
+
+
--- content/pelicanconf.py
+++ content/pelicanconf.py
@@ -17,19 +17,23 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
+    ("Source Code", "https://github.com/Jeffrey-P-McAteer/pelican-playground"),
 )
 
 # Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
+#SOCIAL = (
+#    ("You can add links in your config file", "#"),
+#    ("Another social link", "#"),
+#)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+THEME_TEMPLATES_OVERRIDES = [ 'theme-overrides' ]
+
+STATIC_PATHS = [
+    'images',
+    'static'
+]
